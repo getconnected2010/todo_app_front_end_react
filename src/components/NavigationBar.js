ef28e07dee425c478a29171ae7d4a16dc1d12659
getconnected2010 <65646133+getconnected2010@users.noreply.github.com>
2022-07-02 16:12:46 -0400

    Added an avatar logo to profile

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
+import Image from 'react-bootstrap/Image';
+import brain from '../images/brain.jpg';
 
 const NavigationBar =()=>{
     return(
         <div className="">
             <Navbar className="navigationBar" variant="dark" >
                 <Container>
-                <Navbar.Brand href="/">Tasks</Navbar.Brand>
-                <Nav className="me-auto">
-                <Navbar.Brand href="/add">Add To List</Navbar.Brand>
-                <Navbar.Brand href="/avatar">Upload Avatar</Navbar.Brand>
-                </Nav>
+                    <Nav className="me-auto">
+                        <Navbar.Brand href="/">Tasks</Navbar.Brand>
+                        <Navbar.Brand href="/add">Add To List</Navbar.Brand>
+                        <Navbar.Brand href="/avatar">Upload Avatar</Navbar.Brand>
+                        <Image roundedCircle src= {brain} className="img"/>
+                    </Nav>
                 </Container>
             </Navbar>
         </div>
